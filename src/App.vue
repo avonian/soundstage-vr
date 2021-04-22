d4c72028225a1fd37b64a613e7e9fa5a2f2e374e
Ara Kevonian <ara.kevonian@gmail.com>
2021-04-22 16:51:43 +0200

    Disable support for Safari (Video meshes perform horribly)

--- src/App.vue
+++ src/App.vue
@@ -599,18 +599,19 @@
                                  aria-labelledby="modal-headline">
                                 <div v-if="!browserSupported" class="text-white">
                                     <h3 class="text-3xl font-medium mt-3 w-full text-center mb-6">
-                                        Unsupported browser
+                                        Safari not supported
                                     </h3>
                                     <p class="text-lg mb-4 leading-relaxed">
-                                        We are still working to improve cross-browser support.
+                                        To ensure an adequate experience we ask that you please reconnect with a browser other than Safari.
                                     </p>
                                     <p class="text-lg mb-4 leading-relaxed">
-                                        To ensure an adequate experience we ask that you please reconnect with one of the following web browsers:
+                                        We invite you to try again using one of the following:
                                     </p>
                                     <ul class="text-lg list-disc mt-3 mb-4 ml-8 leading-relaxed">
                                         <li class="mt-3 font-medium"> Google Chrome</li>
                                         <li class="mt-3 font-medium"> Microsoft Edge</li>
                                         <li class="mt-3 font-medium"> Brave</li>
+                                        <li class="mt-3 font-medium"> Firefox</li>
                                     </ul>
                                     <p class="text-lg mb-4 leading-relaxed">
                                         We apologize for the inconvenience.
@@ -847,10 +848,10 @@
     mounted: async function () {
 
       /* Check browser */
-      //if(browser().name !== 'chrome') {
-        //this.browserSupported = false;
-        //return;
-      //}
+      if(browser().name === 'safari') {
+        this.browserSupported = false;
+        return;
+      }
 
       /* Set event configuration */
       await this.initConfig();
