0cd78d53efdd971c363a1f77ac6c7d4958ccc33e
Ara Kevonian <ara.kevonian@gmail.com>
2021-04-25 10:18:23 +0200

    Add bar spotlights for 'high' quality graphics

--- src/customizer.js
+++ src/customizer.js
@@ -20,6 +20,58 @@ export class Customizer {
         galleryPoster.parent = posterGallery;
       }
     }
+    if(this.userSettings.graphicsQuality === 'high') {
+      this.barLights();
+    }
+  }
+  barLights () {
+    let selectionLight = new BABYLON.SpotLight("selectLight", new BABYLON.Vector3(0.864, 2, -6.131),
+      new BABYLON.Vector3(0, -1, 0),BABYLON.Tools.ToRadians(45), 1, this.scene);
+    selectionLight.intensity = 100;
+    selectionLight.angle = BABYLON.Tools.ToRadians(120);
+    selectionLight.diffuse = BABYLON.Color3.Purple();
+
+    let tealLight = selectionLight.clone("tealLight");
+    tealLight.position = new BABYLON.Vector3(-0.6, 1.787, -6.131);
+    tealLight.diffuse = BABYLON.Color3.Teal();
+    /*
+    let purpleLight = selectionLight.clone("purpleLight");
+    purpleLight.position = new BABYLON.Vector3(1.6, 1.787, -6.131);
+    purpleLight.diffuse = BABYLON.Color3.Purple();
+    */
+    let tealLight2 = selectionLight.clone("tealLight2");
+    tealLight2.position = new BABYLON.Vector3(3.25, 1.787, -6.131);
+    tealLight2.diffuse = BABYLON.Color3.Blue();
+
+    const shadowGenerator = new BABYLON.ShadowGenerator(1024, selectionLight);
+    shadowGenerator.usePercentageCloserFiltering = true;
+    //  shadowGenerator.filteringQuality = BABYLON.ShadowGenerator.QUALITY_HIGH;
+
+    this.scene.meshes.forEach(mesh => {
+      shadowGenerator.getShadowMap().renderList.push(mesh);
+      mesh.receiveShadows = true;
+      console.log(mesh.name + " receiveShadows " + mesh.receiveShadows);
+    });
+
+    const shadowGeneratorBlue = new BABYLON.ShadowGenerator(1024, tealLight2);
+    shadowGeneratorBlue.usePercentageCloserFiltering = true;
+    //  shadowGenerator.filteringQuality = BABYLON.ShadowGenerator.QUALITY_HIGH;
+
+    this.scene.meshes.forEach(mesh => {
+      shadowGeneratorBlue.getShadowMap().renderList.push(mesh);
+      mesh.receiveShadows = true;
+      console.log(mesh.name + " receiveShadows " + mesh.receiveShadows);
+    });
+
+    const shadowGeneratorTeal = new BABYLON.ShadowGenerator(1024, tealLight);
+    shadowGeneratorTeal.usePercentageCloserFiltering = true;
+    //  shadowGenerator.filteringQuality = BABYLON.ShadowGenerator.QUALITY_HIGH;
+
+    this.scene.meshes.forEach(mesh => {
+      shadowGeneratorTeal.getShadowMap().renderList.push(mesh);
+      mesh.receiveShadows = true;
+      console.log(mesh.name + " receiveShadows " + mesh.receiveShadows);
+    });
   }
 }
 
