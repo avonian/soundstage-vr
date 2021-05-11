44816b39c2f67c83b18ad61f53ed31aeb211bb13
Ara Kevonian <ara.kevonian@gmail.com>
2021-05-11 19:48:07 +0200

    Update ara's config

--- src/configs/ara.config.js
+++ src/configs/ara.config.js
@@ -10,6 +10,58 @@ export default {
     'moderator': true,
     'stage_controls': true
   },
+  'videos': [{
+    'url': 'https://assets.soundstage.fm/vr/Default.mp4',
+    'label': 'Default'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/kill-paris/Intro-Short.mp4',
+    'label': 'Intro'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/kill-paris/Planetary-Sunrise.mp4',
+    'label': 'Sunrise'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/beat-swiper.mp4',
+    'label': 'Beat Swiper'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Blue-Beams.mp4',
+    'label': 'Blue Beams'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Disco-2.mp4',
+    'label': 'Disco'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/flamboyant-lines.mp4',
+    'label': 'Flamboyant Lines'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Megapixel.mp4',
+    'label': 'Megapixel'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Purple-Tunnel.mp4',
+    'label': 'Purple Tunnel'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Reactor.mp4',
+    'label': 'Reactor'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Retro-1.mp4',
+    'label': 'Waves'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Retro-2.mp4',
+    'label': 'Retro'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Ring-Pulse.mp4',
+    'label': 'Ring Pulse'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/split-sphere.mp4',
+    'label': 'Split Sphere'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Color-Tiler.mp4',
+    'label': 'Tiler'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Cube-Trails.mp4',
+    'label': 'Trails'
+  }, {
+    'url': 'https://assets.soundstage.fm/vr/Ultra.mp4',
+    'label': 'Ultra'
+  }],
   'posters': [
     {
       'name': 'LandscapePoster3',
