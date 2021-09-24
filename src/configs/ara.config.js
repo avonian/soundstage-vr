e15081826dbacf404da197ae51ff3896c088dcae
Ara Kevonian <ara.kevonian@gmail.com>
2021-09-24 19:25:22 +0200

    Update ara's config

--- src/configs/ara.config.js
+++ src/configs/ara.config.js
@@ -4,11 +4,12 @@ export default {
   'space_id': 7,
   'user_id': 1,
   'kiosk': {"poster": "https://assets.soundstage.fm/vr/phuture/kiosk.jpg"},
+  'introAudio': 'Phuture - Intro Minimix Contest.mp3',
   'videos': [{
     'url': 'https://assets.soundstage.fm/vr/phuture/WindowScene.mp4',
     'label': 'Default'
   }, {
-    'url': 'https://assets.soundstage.fm/vr/Intro.mp4',
+    'url': 'https://assets.soundstage.fm/vr/phuture/Intro-Phuture.mp4',
     'label': 'Intro'
   }, {
     'url': 'https://assets.soundstage.fm/vr/illgates/visuals/mindexpander1c.mp4',
@@ -143,5 +144,271 @@ export default {
     'height': 1.9,
     'photo_url': 'https://assets.soundstage.fm/vr/phuture/zenaware2.jpg',
     'rotationY': 90
-  }]
+  }],
+  trackInfo: {
+    'Minnesota - Golden Era.mp3': {
+      'name': 'Minnesota - Golden Era',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/minnesotaBASS/golden-era-187924',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/minnesota/golden-era-mix-v1',
+        },
+        {
+          'name': 'spotify',
+          'url': 'https://open.spotify.com/track/0BrgrPEXlEeqUzHkFfTYeh',
+        }
+      ]
+    },
+    'Wickit-Hate_us_cause_we-stamos.mp3': {
+      'name': 'Wick-It the Instigator - Hate us cuz we stamos',
+      'links': [
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/wick-it/hate-us-cuz-we-stamos',
+        },
+        {
+          'name': 'spotify',
+          'url': 'https://open.spotify.com/track/79qhPIB03MrzIjOgQkJu9Z',
+        }
+      ]
+    },
+    'Phuture Minimix - KOKIO.mp3': {
+      'name': 'KOKIO (Minmix)',
+      'links': [
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/kokiobeats',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - KOKIO.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - BrendaVega.mp3': {
+      'name': 'Brenda Vega (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/brendavega',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/brendaxvega',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - BrendaVega.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - snuggles.mp3': {
+      'name': 'snuggles (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/snuggles',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/imsnuggles',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - snuggles.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - Belak.mp3': {
+      'name': 'Belak & Viletone (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/belak_us',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/belak_us',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - Belak.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - SHOCKFACE.mp3': {
+      'name': 'Shöckface (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/shockfacemusic',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/shockfacemusic',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - SHOCKFACE.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - ELIMINA.mp3': {
+      'name': 'ELIMINA (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/eliminamusic',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/eliminamusic',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - ELIMINA.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - 23-odd-cats.mp3': {
+      'name': '23 Odd Cats (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/23oddcats',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/23oddcats',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - 23-odd-cats.mp3',
+        }
+      ]
+    },
+    'Phuture Minimix - MerricatBlack.mp3': {
+      'name': 'Merricat Black	(Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/merricat_black',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/merricatblack',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - MerricatBlack.mp3'
+        }
+      ]
+    },
+    'Phuture Minimix - ChrizJavey.mp3': {
+      'name': 'chriz javey (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/chrizjavey',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/launchkontrol',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - ChrizJavey.mp3'
+        }
+      ]
+    },
+    'Phuture Minimix - illexotic.mp3': {
+      'name': 'illexotic (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/illexotic',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/illexotic',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - illexotic.mp3'
+        }
+      ]
+    },
+    'Phuture Minimix - Kayoti.mp3': {
+      'name': 'kayoti (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/kayoti',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/kayotimusic',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - Kayoti.mp3'
+        }
+      ]
+    },
+    'Phuture Minimix - L3THO.mp3': {
+      'name': 'L3THO (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/l3tho',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/l3tho',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - L3THO.mp3'
+        }
+      ]
+    },
+    'Phuture Minimix - SYLK.mp3': {
+      'name': 'SYLK (Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/l3tho',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/sylkbeats',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - SYLK.mp3'
+        }
+      ]
+    },
+    'Phuture Minimix - crujii.mp3': {
+      'name': 'Crujii	(Minimix)',
+      'links': [
+        {
+          'name': 'audius',
+          'url': 'https://audius.co/crujii',
+        },
+        {
+          'name': 'soundcloud',
+          'url': 'https://soundcloud.com/crujii',
+        },
+        {
+          'name': 'mp3',
+          'url': 'https://assets.soundstage.fm/vr/phuture/Phuture Minimix - crujii.mp3'
+        }
+      ]
+    }
+  }
 }
